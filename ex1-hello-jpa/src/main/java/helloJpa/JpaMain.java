1f4be28457b8ffaf7845778038293458ee359211
xodlf100 <xodlf100@gmail.com>
2024-11-09 19:37:30 +0900

    feat jpa crud

---
+++ ex1-hello-jpa/src/main/java/helloJpa/JpaMain.java
@@ -0,0 +1,41 @@
+package helloJpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 생성
+//            Member member = new Member();
+//            member.setId(1L);
+//            member.setName("hello");
+//            em.persist(member);
+
+            // 수정
+            Member findMember = em.find(Member.class, 1L);
+            findMember.setName("test");
+
+            // 삭제
+//            em.remove(member);
+
+            tx.commit();
+        } catch(Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
