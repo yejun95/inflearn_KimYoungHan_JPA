50c781c66c831848f8304d63b9a39a30ff77bda6
xodlf100 <xodlf100@gmail.com>
2024-11-13 01:06:37 +0900

    feat 연관관계 맵핑

--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -17,18 +18,8 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 생성
-//            Member member = new Member();
-//            member.setId(1L);
-//            member.setName("hello");
-//            em.persist(member);
 
-            // 수정
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("test");
 
-            // 삭제
-//            em.remove(member);
 
             tx.commit();
         } catch(Exception e) {
