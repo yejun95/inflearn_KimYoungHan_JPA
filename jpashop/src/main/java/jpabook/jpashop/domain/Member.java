dd03c37b54e66c916a08ffcb4cc82f040ff5c2fc
xodlf100 <xodlf100@gmail.com>
2024-11-14 23:57:23 +0900

    feat 고급 매핑 * Inheritance * Discriminator * mappedSuperclass

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Member {
+public class Member extends BaseEntity{
 
     @Id @GeneratedValue //생략하면 auto
     @Column(name = "MEMBER_ID")
